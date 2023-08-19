eabb194f22620692766c5c00f3a91270b91d3ad1
WanjikuN <cikunjoroge4@gmail.com>
2023-08-19 21:21:41 +0300

    Add delete functionality

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "./Task";
 
-function TaskList({tasks}) {
-  
+function TaskList({ tasks }) {
+  const [deletedTasks, setDeletedTasks] = useState([]);
+
+  function handleDelete(index) {
+    const updatedDeletedTasks = [...deletedTasks, index];
+    setDeletedTasks(updatedDeletedTasks);
+  }
+
   return (
     <div className="tasks">
-      {tasks.map((task, index)=> {
-        return <Task key={index} text={task.test} category={task.category}/>
+      {tasks.map((task, index) => {
+        if (!deletedTasks.includes(index)) {
+          return (
+            <Task key={index} text={task.text} category={task.category} handleDelete={() => handleDelete(index)}
+            />
+          );
+        }
+        return null;
       })}
     </div>
   );
